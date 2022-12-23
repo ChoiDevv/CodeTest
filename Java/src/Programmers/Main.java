6fe0b20e2961002f7464565a166f6565d8043efc
ChoiDevv <oinkoink71@naver.com>
2022-12-23 11:00:48 +0900

    commit

--- Java/src/Programmers/Main.java
+++ Java/src/Programmers/Main.java
@@ -1,14 +1,15 @@
 package Programmers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
-        AppearOnce appearOnce = new AppearOnce();
-        System.out.println("한 번만 등장한 문자");
-        System.out.println(appearOnce.appearOnce("abcabcadc"));
+
     }
 }
 
@@ -23,5 +24,48 @@ class AppearOnce {
             }
         }
         return Arrays.stream(answer.split("")).sorted().collect(Collectors.joining());
+    }
+}
+
+// 인덱스 바꾸기
+class ChangeIndex {
+    public String changeIndex(String my_string, int num1, int num2) {
+        String answer = "";
+        String[] a = my_string.split("");
+
+        for (int i = 0; i < a.length; i++) {
+            if (i == num1) {
+                answer += a[num2];
+            }
+            else if (i == num2) {
+                answer += a[num1];
+            }
+            else {
+                answer += a[i];
+            }
+        }
+
+        return answer;
+    }
+}
+
+// 배열 회전시키기
+class ArrayRotation {
+    public List<Integer> arrayRotation(int[] numbers, String direction) {
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        if (Objects.equals(direction, "right")) {
+            answer.add(numbers[numbers.length - 1]);
+            for (int i = 0; i < numbers.length - 1; i++) {
+                answer.add(numbers[i]);
+            }
+        } else {
+            for (int i = 1; i < numbers.length; i++) {
+                answer.add(numbers[i]);
+            }
+            answer.add(numbers[0]);
+        }
+
+        return answer;
     }
 }
