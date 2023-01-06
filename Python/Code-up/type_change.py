38e9fe66674afbdfd4a012ae82961342fb34ae65
ChoiDevv <oinkoink71@naver.com>
2023-01-06 09:45:01 +0900

    commit

---
+++ Python/Code-up/type_change.py
@@ -0,0 +1,5 @@
+# 주민번호 입력받아 형태 바꿔 출력하기
+
+registration_number = input()
+
+print(registration_number.replace("-", ""))
---
+++ Python/Code-up/year_month_date_change.py
@@ -0,0 +1,5 @@
+# 연월일 입력받아 순서 바꿔 출력하기
+
+year, month, date = map(str, input().split('.'))
+
+print(date + "-" + month + "-" + year)
