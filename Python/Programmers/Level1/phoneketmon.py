646663da11f92718be5ba2d1e120424cfed403c1
ChoiDevv <oinkoink71@naver.com>
2023-01-06 17:24:36 +0900

    commit

---
+++ Python/Programmers/Level1/phoneketmon.py
@@ -0,0 +1,13 @@
+# 폰켓몬
+
+def solution(nums):
+    answer = 0
+
+    if len(set(nums)) >= len(nums) // 2:
+        answer = len(nums) // 2
+    else:
+        answer = len(set(nums))
+
+    return answer
+
+print(solution([1,2]))
